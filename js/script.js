86e09c97c86b8f581db3ca5a7b22b790b9d01485
DESKTOP-C5OVSQO\Eslam <eslam.hakem12@gmail.com>
2021-01-16 03:37:46 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+	new Splide( '#card-slider', {
+		perPage    : 1,
+		breakpoints: {
+			600: {
+				perPage: 1,
+			}
+		}
+	} ).mount();
+} );
